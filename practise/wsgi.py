408badf411075d9c76bd0808ddad96924fe0ee07
Alezandah <alexokereke47@gmail.com>
2016-03-27 13:04:10 +0000

    modify and created numerous files

--- practise/wsgi.py
+++ practise/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "practise.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "practise.settings.production")
 
 application = get_wsgi_application()
